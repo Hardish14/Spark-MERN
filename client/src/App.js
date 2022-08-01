89bdefcab64f451b0fe78fc12de92e5eb405e6f8
Hardish14 <hardishavlani2627@gmail.com>
2022-08-01 21:49:42 +0530

    ...

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Auth from "./pages/Auth/Auth";
+// import Scrollbar from "./components/Scrollbar/Scrollbar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/Profile/Profile";
+
+function App() {
+  return (
+    <>
+      {/* <Scrollbar /> */}
+      <div className="App">
+        <div className="blur" style={{top: '-18%' , right: '0'}}></div>
+        <div className="blur" style={{top: '36%' , left:'-8rem'}}></div>
+        {/* <Home /> */}
+        <Profile />
+        {/* <Auth /> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
